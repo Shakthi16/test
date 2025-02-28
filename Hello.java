ae0d5a345b13ace19a1b75a02fba4e62c201dc33
shakthisri <srishakthi799@gmail.com>
2025-02-28 14:25:18 +0530

    Updated Hello.java with a new print statement

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ import java.io.*;
 public class Hello {
     public static void main(String args[]) {
         System.out.println("Git - VCS");
+	System.out.println("After First Commit");
+
     }
 }
-
